b7368a30f2b42d7f0ba3e93280ac50444d220939
Steve Persch <steve.persch@pantheon.io>
2018-05-10 14:04:33 -0500

    Cleaning up tests

--- tests/behat/run-behat.sh
+++ tests/behat/run-behat.sh
@@ -8,4 +8,4 @@ echo "\$options['strict'] = 0;" >> ~/.drush/pantheon.aliases.drushrc.php
 TERMINUS_ENV=$CIRCLE_BUILD_NUM
 
 export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/"}, "Drupal\\DrupalExtension" : {"drush" :   {  "alias":  "@pantheon.'$TERMINUS_SITE'.'$TERMINUS_ENV'" }}}}'
-./vendor/behat/behat/bin/behat --config=behat/behat-pantheon.yml
+./vendor/bin/behat --config=behat/behat-pantheon.yml
