acc014617c573789e9ab6c072084a809d35e6607
Steve Persch <steve.persch@pantheon.io>
2018-05-16 17:22:07 -0500

    whitespace

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -11,7 +11,8 @@ use Drupal\DrupalExtension\Context\RawDrupalContext;
 /**
  * Define application features from the specific context.
  */
-class FeatureContext extends RawDrupalContext implements Context, SnippetAcceptingContext {
+class FeatureContext extends RawDrupalContext implements Context, SnippetAcceptingContext
+{
   /**
    * Initializes context.
    * Every scenario gets its own context object.
@@ -19,29 +20,30 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
    * @param array $parameters
    *   Context parameters (set them in behat.yml)
    */
-  public function __construct(array $parameters = []) {
-    // Initialize your context here
-  }
+    public function __construct(array $parameters = [])
+    {
+        // Initialize your context here
+    }
 
   /** @var \Drupal\DrupalExtension\Context\MinkContext */
-  private $minkContext;
+    private $minkContext;
   /** @BeforeScenario */
-  public function gatherContexts(BeforeScenarioScope $scope)
-  {
-      $environment = $scope->getEnvironment();
-      $this->minkContext = $environment->getContext('Drupal\DrupalExtension\Context\MinkContext');
-  }
+    public function gatherContexts(BeforeScenarioScope $scope)
+    {
+        $environment = $scope->getEnvironment();
+        $this->minkContext = $environment->getContext('Drupal\DrupalExtension\Context\MinkContext');
+    }
 
     /**
      * @Given there are :numnber_of_nodes article nodes with a huge number of taxonomy terms each
      */
     public function thereAreArticleNodesWithAHugeNumberOfTaxonomyTermsEach($number_of_nodes)
     {
-       $i = 0;
-       while ($i < $number_of_nodes) {
-           $this->WhenIGenerateAnArticleWithLotsOfTerms();
-           $i++;
-       }
+        $i = 0;
+        while ($i < $number_of_nodes) {
+            $this->WhenIGenerateAnArticleWithLotsOfTerms();
+            $i++;
+        }
     }
 
     /**
@@ -60,12 +62,12 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
     /**
      * Generates a long string of tags used on node add form.
      */
-    private function generateRandomTaxonomyString() {
+    private function generateRandomTaxonomyString()
+    {
         $all_letters = explode(' ', 'a b c d e f g h i j k l m n o p q r s t u v w x y z');
         $i = 0;
         $random_three_letter_combos = array();
-        while($i < 250) {
-
+        while ($i < 250) {
             $random_three_letter_combos[] = $all_letters[rand(0, 25)] . $all_letters[rand(0, 25)] . $all_letters[rand(0, 25)];
             $i++;
         }
