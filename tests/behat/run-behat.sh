360def39a828c952ea1696d74c82093c95af8693
Steve Persch <steve.persch@pantheon.io>
2020-04-29 22:30:45 -0500

    move the env setting

--- tests/behat/run-behat.sh
+++ tests/behat/run-behat.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-
-# TERMINUS_ENV=$CIRCLE_BUILD_NUM
-
-terminus drush $TERMINUS_SITE.$TERMINUS_ENV -- status
-
 export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "https://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/"}}}'
 ./vendor/bin/behat --config=tests/behat/behat-pantheon.yml --strict --stop-on-failure
