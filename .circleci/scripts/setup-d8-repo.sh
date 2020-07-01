38956e6c63f93619e91c7b9b12c25de65e92bdbf
Steve Persch <steve.persch@pantheon.io>
2020-07-01 11:18:25 -0500

    working on scripts

---
+++ .circleci/scripts/setup-d8-repo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+export TERMINUS_ENV=$CIRCLE_BUILD_NUM
+
+# Bring the code down to Circle so that modules can be added via composer.
+git clone $(terminus connection:info ${TERMINUS_SITE}.${TERMINUS_ENV} --field=git_url) drupal-site
+cd drupal-site
+git checkout $TERMINUS_ENV
+
+composer -- config repositories.papc vcs git@github.com:pantheon-systems/pantheon_advanced_page_cache.git
+composer -- require drupal/views_custom_cache_tag "drupal/pantheon_advanced_page_cache:dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
+
+# Don't commit a submodule
+rm -rf web/modules/contrib/pantheon_advanced_page_cache/.git/
+
+# Make a git commit
+git add .
+git commit -m 'Result of build step'
+git push --set-upstream origin $TERMINUS_ENV
