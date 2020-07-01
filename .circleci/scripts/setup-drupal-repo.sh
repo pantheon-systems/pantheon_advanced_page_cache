9b9c06e1c35cf29390b2f7bcf0b0b5b6d5344291
Steve Persch <steve.persch@pantheon.io>
2020-07-01 11:58:06 -0500

    more troubleshooting errors

--- .circleci/scripts/setup-drupal-repo.sh
+++ .circleci/scripts/setup-drupal-repo.sh
@@ -4,9 +4,8 @@ set -e
 export TERMINUS_ENV=$CIRCLE_BUILD_NUM
 
 # Bring the code down to Circle so that modules can be added via composer.
-git clone $(terminus connection:info ${TERMINUS_SITE}.dev --field=git_url) drupal-site
+git clone $(terminus connection:info ${TERMINUS_SITE}.dev --field=git_url) drupal-site  --branch $TERMINUS_ENV
 cd drupal-site
-git checkout $TERMINUS_ENV
 
 composer -- config repositories.papc vcs git@github.com:pantheon-systems/pantheon_advanced_page_cache.git
 composer -- require drupal/views_custom_cache_tag "drupal/pantheon_advanced_page_cache:dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
@@ -15,8 +14,6 @@ composer -- require drupal/views_custom_cache_tag "drupal/pantheon_advanced_page
 rm -rf web/modules/contrib/pantheon_advanced_page_cache/.git/
 
 # Make a git commit
-git config --global user.email "$GitEmail"
-git config --global user.name "Circle CI"
 git add .
 git commit -m 'Result of build step'
 git push --set-upstream origin $TERMINUS_ENV
