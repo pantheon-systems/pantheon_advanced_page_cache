fbceb31657ef6b0a13b91ac163df462989664df0
Steve Persch <stevector@users.noreply.github.com>
2020-07-02 09:28:57 -0500

    Update setup-drupal-repo.sh

--- .circleci/scripts/setup-drupal-repo.sh
+++ .circleci/scripts/setup-drupal-repo.sh
@@ -3,10 +3,12 @@ set -e
 export TERMINUS_ENV=$CIRCLE_BUILD_NUM
 
 # Bring the code down to Circle so that modules can be added via composer.
-git clone $(terminus connection:info ${TERMINUS_SITE}.dev --field=git_url) drupal-site  --branch $TERMINUS_ENV
+git clone $(terminus connection:info ${TERMINUS_SITE}.dev --field=git_url) --branch $TERMINUS_ENV drupal-site
 cd drupal-site
 
+
 composer -- config repositories.papc vcs git@github.com:pantheon-systems/pantheon_advanced_page_cache.git
+# Composer require the given commit of this module
 composer -- require drupal/views_custom_cache_tag "drupal/pantheon_advanced_page_cache:dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
 
 # Don't commit a submodule
