514a55fe890f1f9192837763d8c3988da10f2d46
Roshny Kunjappan <roshnykunjappan@pantheon.io>
2026-07-31 16:14:08 -0400

    [SITE-6015] Fix CDN purge for renamed list tags

---
+++ tests/src/Unit/PantheonEdgeFunctionsMock.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Global-namespace mock for pantheon_clear_edge_keys().
+ *
+ * Loaded by CacheTagsInvalidatorTest to capture CDN purge calls.
+ */
+
+if (!function_exists('pantheon_clear_edge_keys')) {
+
+  /**
+   * Mock implementation that records calls for test assertions.
+   */
+  function pantheon_clear_edge_keys(array $keys) {
+    \Drupal\Tests\pantheon_advanced_page_cache\Unit\CacheTagsInvalidatorTest::$capturedEdgeKeys = $keys;
+  }
+
+}
