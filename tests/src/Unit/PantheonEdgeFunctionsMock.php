77bc14d5a98235c4f5e1f4d094542e78623ebc8c
Roshny Kunjappan <roshnykunjappan@pantheon.io>
2026-08-11 10:47:36 -0400

    [SITE-6015] Fix PHPCS lint: use statement for namespaced class ref
    SITE-6015

--- tests/src/Unit/PantheonEdgeFunctionsMock.php
+++ tests/src/Unit/PantheonEdgeFunctionsMock.php
@@ -7,13 +7,15 @@
  * Loaded by CacheTagsInvalidatorTest to capture CDN purge calls.
  */
 
+use Drupal\Tests\pantheon_advanced_page_cache\Unit\CacheTagsInvalidatorTest;
+
 if (!function_exists('pantheon_clear_edge_keys')) {
 
   /**
    * Mock implementation that records calls for test assertions.
    */
   function pantheon_clear_edge_keys(array $keys) {
-    \Drupal\Tests\pantheon_advanced_page_cache\Unit\CacheTagsInvalidatorTest::$capturedEdgeKeys = $keys;
+    CacheTagsInvalidatorTest::$capturedEdgeKeys = $keys;
   }
 
 }
